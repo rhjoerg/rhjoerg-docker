adb1438e2a787f3dd49c42c8026c6a03393bcfc6
rhjoerg <rhjoerg@gmail.com>
2020-10-23 09:20:56 +0200

    proof-of-concept

--- rhjoerg-docker-maven/src/main/java/ch/rhjoerg/docker/maven/MavenServer.java
+++ rhjoerg-docker-maven/src/main/java/ch/rhjoerg/docker/maven/MavenServer.java
@@ -1,9 +1,21 @@
 package ch.rhjoerg.docker.maven;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class MavenServer
 {
 	public static void main(String[] args)
 	{
+		try
+		{
+			Path dataPath = Path.of("/data");
 
+			Files.walk(dataPath).forEach(p -> System.out.println(p));
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
